1f7d681b7c6eb4f52908d0281e63a48197025097
Lucas Taliberti de Sousa <lucastaliberti@gmail.com>
2018-04-07 18:24:23 -0700

    Initial commit, based on eol-last

---
+++ index.js
@@ -0,0 +1,11 @@
+
+"use strict";
+
+module.exports = {
+    rules: {
+        "bof-newline": require("./lib/rules/bof-newline")
+    },
+    rulesConfig: {
+        "bof-newline": "always"
+    }
+};
